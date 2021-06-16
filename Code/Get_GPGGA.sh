8f4598c67c333189d76f0a950b0175211072a31e
joanmanbar <35076815+joanmanbar@users.noreply.github.com>
2021-06-16 18:28:02 -0500

    Update Get_GPGGA.sh
    NEVER HAVE AN EMPTY SPACE WHEN DEFINING A VARIABLE IN BASH!!!

--- Code/Get_GPGGA.sh
+++ Code/Get_GPGGA.sh
@@ -3,7 +3,7 @@
 # Get variable's length
 ArgSize=$(expr length $1)
 
-# Verify if argument was given as date (e.g. 2014_05_24, or fullpath)
+# Verify if argument has 10 characters as in "2021_05_24", for example, or is a fullpath (first one recommended)
 if [ $ArgSize -eq 10 ]
 then
   Folder="/home/pi/Desktop/GreenCover/Output/$1"
@@ -14,11 +14,11 @@ fi
 # Verify if output folder exists (create it otherwise)
 [ ! -d "$Folder/GPGGA" ] && mkdir -p "$Folder/GPGGA"
 
-# Outfile
-Outfile1 = "/home/pi/Desktop/GreenCover/Output/$1/GPGGA/$1.txt"
-Outfile2 = "/home/pi/Desktop/GreenCover/Output/$1/GPGGA/curated1_$1.txt"
+# Files names for OrganizeGPGGA.py 
+Outfile1="/home/pi/Desktop/GreenCover/Output/$1/GPGGA/$1.txt"
+Outfile2="/home/pi/Desktop/GreenCover/Output/$1/GPGGA/curated1_$1.txt"
 
-# Extract all lines containing "GPGGA" in all files and put them in a separate file
+# Extract all lines containing "GPGGA" in all GPS files and put them in a separate file
 cat -e $Folder/Files/*.txt | grep '$GPGGA' > $Outfile1
 
 # Run python code to organize file into columns
